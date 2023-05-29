3e01040e8e26ab67fc5908005b6583e267c050ce
GmalFari <gmalfari926@gmail.com>
2023-05-29 20:05:55 +0300

    a

--- pages/api/_app.js
+++ pages/api/_app.js
@@ -1,22 +1,22 @@
-// import Router from "next/router";
-// import Head from "next/head";
-// import { ChakraProvider } from "@chakra-ui/react";
-// import Layout from "../../components/Layout";
+import Router from "next/router";
+import Head from "next/head";
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "../../components/Layout";
 
-// function MyApp({Component,pageProps}){
-//     return (
-//         <>
-//         <Head>
+function MyApp({Component,pageProps}){
+    return (
+        <>
+        <Head>
 
-//         </Head>
-//         <ChakraProvider>
-//             <Layout>
-//                 <Component {...pageProps} />
-//             </Layout>
-//         </ChakraProvider>
-//         </>
-//     );
-// }
+        </Head>
+        <ChakraProvider>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ChakraProvider>
+        </>
+    );
+}
 
 
-// export default MyApp
+export default MyApp
